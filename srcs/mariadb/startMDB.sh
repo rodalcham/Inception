f2e00de0fed9b26e4a9130d8a8b16e0ed263d4ac
rodalcham <142218652+rodalcham@users.noreply.github.com>
2025-05-01 17:51:30 +0200

    Started mariadb and docker-compose

---
+++ srcs/mariadb/startMDB.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+service mysql start
+
+until mysqladmin ping --silent; do
+	echo "Waiting for MySQL..."
+	sleep 1
+done
+
+mysql -e "CREATE DATABASE IF NOT EXISTS ${database_name};"
+mysql -e "CREATE USER '${mysql_user}'@'%' IDENTIFIED BY '${mysql_password}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${database_name}.* TO '${mysql_user}'@'%';"
+mysql -u${mysql_root_user} -p${mysql_root_password} -e "ALTER USER '${mysql_root_user}'@'localhost' IDENTIFIED BY '${mysql_root_password}';"
+mysql -e "FLUSH PRIVILEGES;"
+mysqladmin -u${mysql_root_user} -p${mysql_root_password} shutdown
+
+exec mysqld_safe
