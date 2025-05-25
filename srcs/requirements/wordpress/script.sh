3f714f2db3107ef69c70938322f5830dc5336a12
rodalcham <142218652+rodalcham@users.noreply.github.com>
2025-05-25 15:40:10 +0200

    trying new conf

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -18,7 +18,7 @@ else
 	cd /var/www/html/wordpress
 	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/localhost/$DOMAIN_NAME/g" wp-config-sample.php
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 	mv wp-config-sample.php wp-config.php
 
